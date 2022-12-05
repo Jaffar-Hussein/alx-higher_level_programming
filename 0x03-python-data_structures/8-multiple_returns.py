3e27868012120b8843c8cd2ad785b8c94218f1e5
Jaffar Hussein <jaffar.gura@universite-paris-saclay.fr>
2022-12-05 20:24:13 +0000

    function that finds the biggest integer of a list

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-multiple_returns = __import__('8-multiple_returns').multiple_returns
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
